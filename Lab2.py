893a0d7681db2651473bb098a8acdf30900a83b6
rellis14 <106385875+rellis14@users.noreply.github.com>
2022-06-11 14:33:50 -0400

    Create Lab2.py

---
+++ Lab2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+def printVars:
+
+
+var1 = sys.argv[1]
+var2 = sys.argv[2]
+
+print('Variable1 = ' + var1)
+print('Variable2 = ' + var2)
+print('The script and variables used are', str(sys.argv))
